f6b7bc624dd90e210d8261aea9344ef1d4ed37bd
suraj-thadarai <42627217+suraj-thadarai@users.noreply.github.com>
2024-02-07 10:49:52 +0545

    updated Card.js for scale transformation

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Card = ({ name, image, powerStats }) => {  
   return (
-    <div className='w-[250px] h-[300px] bg-sky-300 rounded-md shadow-lg p-2 overflow-hidden'>
+    <div className='w-[250px] h-[300px] bg-sky-300 rounded-md shadow-lg p-2 overflow-hidden hover:cursor-pointer hover:scale-105'>
       <div className='flex flex-col items-center'>
         <img className='w-[190px] h-[240px] p-3 rounded-2xl object-cover' src={image} alt={image}/>
         <h2 className='text-2xl font-bold'>{name}</h2>
